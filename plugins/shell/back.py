549a5a179962d4d5bd89c5ed64805c5d8c887b19
theau <theau.trova@etu.univ-lyon1.fr>
2021-10-29 00:43:27 +0200

    choose user to execute command, prepare permission

--- plugins/shell/back.py
+++ plugins/shell/back.py
@@ -6,22 +6,32 @@ import json
 class ShellBack(tools.module_structure.TABModuleBack):
     def __init__(self, app):
         super().__init__(app)
+        self._add_route("execute/<user>", "POST", self._execute)
         self._add_route("execute", "POST", self._execute)
         self.history = []
         
-    def _execute(self):
+    def _execute(self, user=None):
         request_parameters = json.load(bottle.request.body)
+        
+        if user is None and "user" in request_parameters:
+            user = request_parameters["user"]
+        elif user is None:
+            return json.dumps({"state":"not ok","result":"no user provided"})
+        
         if "command" in request_parameters:
-            command = request_parameters["command"]
-            result = os.popen(command).read()
-            self.history.append( {"command": command, "result": result} )
-            return json.dumps({"state":"ok","result":result})
+            return json.dumps( self.__execute_command(user, request_parameters["command"]))
         else:
             return json.dumps({"state":"not ok","result":"no command provided"})
+
+    def __execute_command(self, user, command):
+        if user != "root": command = f'su {user} -s "/bin/sh" -c "{command}"'
+        result = os.popen(command).read()
+        self.history.append( {"user": user, "command": command, "result": result} )
+        return {"state":"ok","result":result}
 
     def status(self):
         return json.dumps({"state":"Loaded", "history": self.history})
     
     def rights(self):
-        rights = ["shell.execute"]
+        rights = ["shell._execute_root", "shell._execute"]
         return json.dumps(rights)
